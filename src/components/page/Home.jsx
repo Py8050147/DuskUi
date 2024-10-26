c7f24435f7b5d15c50a5ec083c6eaf5b02f26b6a
Pankaj Kumar <94382374+Py8050147@users.noreply.github.com>
2024-10-26 15:17:50 +0530

    "Updated Home component with new styling and layout"

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -1,13 +1,16 @@
-import GetAllProduct from "../GetAllProduct"
-
+import GetAllProduct from "../GetAllProduct";
 
 const Home = () => {
   return (
-    <div>
-      <h1>welcome to home</h1>
-      <GetAllProduct />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-gray-400 ">
+      <div className="max-w-2xl lg:max-w-full w-full p-6 bg-black/40 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold text-center mb-4 text-blue-600">
+          Welcome to Home
+        </h1>
+        <GetAllProduct />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
